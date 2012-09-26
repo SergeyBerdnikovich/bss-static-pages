1536fc602751b3eb186167082336e12715adfe7f
Sergey Berdnikovich <sergey.berdnikovich@altoros.com>
2012-09-26 18:41:54 +0300

    updated valid

--- app/models/email.rb
+++ app/models/email.rb
@@ -3,10 +3,10 @@ class Email < ActiveRecord::Base
 
   validates :name, :length => { :minimum => 3, :maximum => 15, },
   				   presence: true
-  validates :surname, :length => { :minimum => 3, :maximum => 15, },
+  validates :surname, :length => { :minimum => 3, :maximum => 20, },
   					  presence: true
   validates :email, presence: true,
-  					:length => { :minimum => 3, :maximum => 15, },
+  					:length => { :minimum => 3, :maximum => 50, },
   					:uniqueness => true,
            			:format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end

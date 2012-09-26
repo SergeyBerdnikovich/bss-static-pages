5b7d2b0a7122a5e5349a610b1d5d8ea7e97dcfe6
Sergey Berdnikovich <sergey.berdnikovich@altoros.com>
2012-09-26 12:26:46 +0300

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BssStaticPages::Application.config.secret_token = '008bf3cf90662547a893bd5fce3678d65c4e22b28783026303fd9c8cd53b1805ffad999bce238640ceda6827c3f3e9dcdb38b73eb95e0888095f6047e8ed2ee1'
